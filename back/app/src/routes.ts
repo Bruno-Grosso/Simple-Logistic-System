d5e31bcbc417b2f559d89533cccfdbdaf24add0b
Bruno-Grosso <bruno.grosso@grad.iprj.uerj.br>
2026-05-22 00:42:11 -0300

    Update routes.ts

--- back/app/src/routes.ts
+++ back/app/src/routes.ts
@@ -2,19 +2,31 @@
   Prototype file, ignore unless you are Bruno Sergio
 */
 
+import { orders, warehouses } from "./controller"; 
+
 export async function handleRoutes(req: Request) {
   const url = new URL(req.url);
 
   if (url.pathname === "/route" && req.method === "POST") {
-    try {
-      const body = await req.json() as { locations: any };
+    try {      
+      const body = await req.json() as { orderId: string, warehouseId: string };
+      const orderData = await orders.byId(body.orderId);
+      const warehouseData = await warehouses.byId(body.warehouseId);
 
-      // Note: This requires the motor started by setup_valhalla.sh to be running
+      if (orderData.length === 0 || warehouseData.length === 0) {
+        return new Response("Pedido ou Armazém não encontrados", { status: 404 });
+      }
+
+      const valhallaLocations = [
+        { lat: Number(warehouseData[0].latitude), lon: Number(warehouseData[0].longitude) },
+        { lat: Number(orderData[0].latitude), lon: Number(orderData[0].longitude) }
+      ];
+
       const valhallaRes = await fetch("http://localhost:8002/route", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          locations: body.locations,
+          locations: valhallaLocations, 
           costing: "truck",
           units: "kilometers",
           language: "en-US",
