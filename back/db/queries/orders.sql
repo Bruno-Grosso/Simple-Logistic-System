fb36accbfcc73b2edc5350951510a52ec18be9a5
Bruno Sérgio Grosso <bruno.grosso@grad.iprj.uerj.br>
2026-08-20 18:27:49 -0300

    Update orders.sql

--- back/db/queries/orders.sql
+++ back/db/queries/orders.sql
@@ -6,3 +6,8 @@ SELECT * FROM orders WHERE id = $1;
 
 -- Get orders by client ID
 SELECT * FROM orders WHERE client_id = $1;
+
+-- Update order distance in km from Valhalla
+UPDATE orders 
+SET distance_km = $1 
+WHERE id = $2;
