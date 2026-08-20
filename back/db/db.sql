7499ab06dfb4c496fa592581ae355a92072aefe7
Bruno Sérgio Grosso <bruno.grosso@grad.iprj.uerj.br>
2026-08-20 19:52:22 -0300

    Update db.sql

--- back/db/db.sql
+++ back/db/db.sql
@@ -88,7 +88,7 @@ CREATE TABLE orders (
     status TEXT CHECK(status IN ('Pending','Shipped','Delivered','Canceled')) DEFAULT 'Pending',
     supplier_id TEXT REFERENCES suppliers(id),
     supplier_delivery INTEGER NOT NULL DEFAULT 0,
-    distance_km NUMERIC(10, 2)
+    distance_km REAL
 );
 
 CREATE TABLE orders_items (
